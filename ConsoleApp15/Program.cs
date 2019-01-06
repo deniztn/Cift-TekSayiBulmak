3701bf8ae481dceaa8abee3bdbe47d316534904d
Erdeniz Tunç <erdeniztunc74@hotmail.com>
2019-01-06 20:20:31 +0300

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("bir sayi giriniz:");
+            int sayi = Convert.ToInt16(Console.ReadLine());
+            int mod = sayi % 2;
+            if (mod == 0)
+                Console.Write("sayi çift.");
+            else
+                Console.Write("sayi tek.");
+            Console.ReadKey();
+        }
+    }
+}
